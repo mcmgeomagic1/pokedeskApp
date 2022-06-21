d23bd95d1499e4a3d5849b3a6f2bac87de3967a9
Mário Monteiro da Costa <59767266+mcmgeomagic1@users.noreply.github.com>
2022-06-21 16:55:47 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,45 @@
+import {useEffect, useState, useRef} from "react"
+import {ThemeProvider} from "styled-components"
+import GlobalStyles from './Styled/Global.js'
+import Home from './components/Home'
+
+export default function App() {
+  const [pokemonData, setPokemonData] = useState([])
+  const [filteredPokemon, setFilteredPokemon] = useState([])
+  console.log(pokemonData)
+  
+
+  const api = "https://unpkg.com/pokemons@1.1.0/pokemons.json"
+
+  useEffect(()=> {
+    fecthPokemon()
+  }, [])
+
+  const fecthPokemon = async ()=> {
+    const data = await fetch(api)
+    const pokemons = await data.json()
+    setPokemonData(pokemons.results)
+    setFilteredPokemon(pokemons.results)
+  }
+
+  if(pokemonData.length == 0) return (
+    <div style={{display: "flex", height: "100vh", justifyContent: "center", alignItems: "center"}}>
+      <h1>Loading...</h1>
+    </div>
+  )
+
+  const theme = {
+    mobile: '768px',
+    tablet: '1394px',
+    smallerDevices: '477px'
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Home pokemonData={pokemonData} filteredPokemon={filteredPokemon} setFilteredPokemon={setFilteredPokemon}/>
+      </>
+    </ThemeProvider>
+  );
+}
